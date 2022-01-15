ada44d0ba48aeeba51051f52a0aae8c16858e4aa
cyh0530 <chiayanghuang0@gmail.com>
2022-01-15 01:37:36 -0800

    feat: add about us section

---
+++ docs/src/pages/Index/AboutUs/index.tsx
@@ -0,0 +1,80 @@
+import {
+    Container,
+    Typography,
+    Grid,
+    Card,
+    CardMedia,
+    CardContent,
+    CardActions,
+    Button,
+} from "@mui/material";
+
+const AboutUs = () => {
+    return (
+        <Container maxWidth="lg">
+            <Typography variant="h3" align="center" sx={{marginBottom: "1em"}}>
+                About Us
+            </Typography>
+            <Grid container>
+                {ABOUT.map((person) => (
+                    <Grid item xs={12} lg={4}>
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardMedia
+                                component="img"
+                                image={(person.image)}
+                                alt={person.name}
+                            />
+                            <CardContent>
+                                <Typography
+                                    gutterBottom
+                                    variant="h5"
+                                    component="div"
+                                >
+                                    {person.name}
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    color="text.secondary"
+                                >
+                                    {person.description}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <a
+                                    href={person.link}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <Button size="small">Learn More</Button>
+                                </a>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    );
+};
+
+const ABOUT = [
+    {
+        name: "Jolin Tsai",
+        image: "static/images/AboutUs/jolin.jpg",
+        description: "Hi, I am an undergrad CS student in UW",
+        link: "https://linkedin.com",
+    },
+    {
+        name: "Peter Chan",
+        image: "static/images/AboutUs/peter.jpg",
+        description: "Hi, I am an undergrad CS student in UW",
+        link: "https://linkedin.com",
+    },
+    {
+        name: "Justin Huang",
+        image: "static/images/AboutUs/justin.jpg",
+        description: "Hi, I am an undergrad CS student in UW",
+        link: "https://linkedin.com",
+    },
+];
+
+export default AboutUs;
--- docs/src/pages/Index/index.tsx
+++ docs/src/pages/Index/index.tsx
@@ -1,9 +1,10 @@
 import Banner from './Banner'
-
+import AboutUs from './AboutUs'
 const Index = () => {
     return (
         <div>
             <Banner />
+            <AboutUs />
         </div>
     )
 }
