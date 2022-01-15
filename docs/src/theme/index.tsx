14a60097ea717277d50ddef0ec65802a97f3b356
cyh0530 <chiayanghuang0@gmail.com>
2022-01-14 21:45:01 -0800

    feat: add theme color

---
+++ docs/src/theme/index.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#4b2e83", // uw purple
+    },
+    secondary: {
+      main: '#b7a57a', // uw gold
+    },
+  },
+});
+
+export default theme;
