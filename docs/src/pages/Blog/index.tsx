d19068709f714e8ecda221e8b02e87f026fdad62
cyh0530 <chiayanghuang0@gmail.com>
2022-01-27 12:58:30 -0800

    feat: add week 3 blog

--- docs/src/pages/Blog/index.tsx
+++ docs/src/pages/Blog/index.tsx
@@ -4,26 +4,32 @@ import { Link } from "react-router-dom";
 const WEEK = 3;
 
 const Blog = () => {
+    const blogs = []
+    for (let i = WEEK; i >= 1; i--) {
+        blogs.push(<Grid item xs={12} md={4}>
+            <Link to={"/blogs/" + i}>
+                <Card sx={{ maxWidth: 345, margin: "auto" }}>
+                    <CardContent>
+                        <Typography
+                            gutterBottom
+                            variant="h5"
+                            component="div"
+                        >
+                            {"Week " + i}
+                        </Typography>
+                        <Typography variant="h6" color="text.secondary">
+                            {`Week ${i} Summary`}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Link>
+        </Grid>)
+    }
+
     return (
         <Grid container justifyContent="center" spacing={2}>
-            <Grid item xs={12} md={4}>
-                <Link to={"/blogs/1"}>
-                    <Card sx={{ maxWidth: 345, margin: "auto" }}>
-                        <CardContent>
-                            <Typography
-                                gutterBottom
-                                variant="h5"
-                                component="div"
-                            >
-                                {"Week 1"}
-                            </Typography>
-                            <Typography variant="h6" color="text.secondary">
-                                {"Week 1 Summary"}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Link>
-            </Grid>
+            {blogs}
+            
         </Grid>
     );
 };
