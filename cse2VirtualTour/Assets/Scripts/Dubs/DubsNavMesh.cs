1fb677ef734bcbfd57532a449617c48dc998d960
Peter Chia-Hao Chan <ch904@uw.edu>
2022-02-17 02:43:00 +0800

    add path finding for dub

---
+++ cse2VirtualTour/Assets/Scripts/Dubs/DubsNavMesh.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DubsNavMesh : MonoBehaviour
+{
+    [SerializeField] private Transform movePositionTransform;
+    [SerializeField] private Transform CameraPositionTransform;
+    [SerializeField] private float EPSILON;
+    Animator mAnimator;
+    private NavMeshAgent navMeshAgent;
+    private void Awake()
+    {
+        mAnimator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+   
+    private void Update()
+    {
+  
+        float angle = movePositionTransform.localEulerAngles.y + CameraPositionTransform.localEulerAngles.y;
+        Vector3 mLookDirection;
+        mLookDirection.x = movePositionTransform.position.x + Mathf.Sin(angle * Mathf.Deg2Rad) * 2.5f;
+        mLookDirection.z = movePositionTransform.position.z + Mathf.Cos(angle * Mathf.Deg2Rad) * 2.5f;
+        mLookDirection.y = movePositionTransform.position.y;
+      
+
+        if ((transform.position - mLookDirection).magnitude > EPSILON )
+        {
+            mAnimator.SetBool("IsMoving", true);
+
+            navMeshAgent.destination = mLookDirection;
+        }
+        else
+        {
+            mAnimator.SetBool("IsMoving", false);
+          
+        }
+        
+    }
+}
