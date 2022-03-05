2407454945f49ca1d4efeba01eb7f2ba95387aec
Peter Chia-Hao Chan <ch904@uw.edu>
2022-03-06 02:33:17 +0800

    fixed typo

--- cse2VirtualTour/Assets/Scripts/Advisor/Message.cs
+++ cse2VirtualTour/Assets/Scripts/Advisor/Message.cs
@@ -9,7 +9,7 @@ public static class Message
     public const string HIGH_SCHOOL_INTRO = "Thank you for your interest in the Paul G. Allen School of Computer Science & Engineering. You will be considered for Direct To Major (DTM) admission in the Allen School if you select Computer Science or Computer Engineering as the first-choice major on your application.";
 
     public const string HS_Q_ADMIN_RATE = "What is the admission rate for the Allen School?";
-    public const string HS_A_ADMIN_RATE = "In 2021,\n1732 WA residents applied and 456 were admitted (26.3%).\n3887 Domestic Non-Resident applied and 99 were admitted (2.5%).\n1117 international students applied and 43 were applied (3.84%).";
+    public const string HS_A_ADMIN_RATE = "In 2021,\n1732 WA residents applied and 456 were admitted (26.3%).\n3887 Domestic Non-Resident applied and 99 were admitted (2.5%).\n1117 international students applied and 43 were admitted (3.84%).";
     public const string HS_Q_PROGRAM_EXP = "Do I need to have programming experience in high school?";
     public const string HS_A_PROGRAM_EXP = "No, we do not expect freshman applicants to have any programming experience. We look for those students who demonstrate academic excellence and leadership. We look for students with strong academic motivation, who have excelled in math, science, English, and other writing courses.";
     public const string HS_Q_GPA = "Is there a minimum GPA to be competitive for admission?";
