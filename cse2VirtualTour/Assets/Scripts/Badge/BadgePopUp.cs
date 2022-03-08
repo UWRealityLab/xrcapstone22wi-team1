9f2867409f6d00d6fd767ab843f1afc864c83fdc
Jolin Tsai <ylt1215@cs.washington.edu>
2022-03-07 20:51:20 -0800

    fix typo and rebake navigation

--- cse2VirtualTour/Assets/Scripts/Badge/BadgePopUp.cs
+++ cse2VirtualTour/Assets/Scripts/Badge/BadgePopUp.cs
@@ -18,7 +18,7 @@ public class BadgePopUp : MonoBehaviour
     bool quizTaskCoompleted;
     bool alreadyShowDialogue;
 
-    private const string TASK_COMPLETE_DIALOGUE = "Congratulation! You've completed all the tasks for this virtual tour! Now you are given a Husky card as a badge to unlock the entry to the undergrad commons to explore more!";
+    private const string TASK_COMPLETE_DIALOGUE = "Congratulations! You've completed all the tasks for this virtual tour! Now you are given a Husky card as a badge to unlock the entry to the undergrad commons to explore more!";
     // Start is called before the first frame update
     void Start()
     {
